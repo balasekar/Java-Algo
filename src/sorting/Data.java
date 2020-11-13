4f4ecbc2f94a3ba7d39b98e89561e20a855def92
Bala <baalasekar@gmail.com>
2020-11-13 07:17:12 +0000

    Heap Sort implemented

---
+++ src/sorting/Data.java
@@ -0,0 +1,15 @@
+package sorting;
+
+public class Data {
+
+    public int key;
+
+    public Data(int key) {
+        this.key = key;
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(key);
+    }
+}
